5bdd84c0af6f22b7c6c60d758a8d20a9076da593
adrianoaap <ayrespalmeiraa@gmail.com>
2019-03-21 21:32:44 -0300

    Conceitos de variáveis e escopo em js

---
+++ desafio.js
@@ -0,0 +1,23 @@
+// Declare uma variavel chamada 'myvar' sem valor
+var myvar;
+
+// Após declarada atribua o valor 10 a variável 'myvar'
+myvar = 10;
+
+// Declare uma nova variável chamada 'soma', e adcione uma instrução somando os valores '15' e '8'
+var soma = 15 + 8;
+
+// Atribua o valor 'soma' todo o valor dela, somando 1, usando o operador de soma abreviado
+soma++;
+
+// Atribua o valor 'soma' todo o valor dela, multiplicando 3, usando o operador de soma abreviado
+soma*=3
+
+// Qual o valor da variável soma até aqui
+57
+
+// Declare a variável 'sou_ninja', atribuindo a ela o valor boolean que representa verdadeiro
+var sou_ninja= true;
+
+// Declare a variável 'comida' que receba um array com os valores 'arroz', 'feijão' e 'ovo'
+var comida = ['arroz', 'feijão', 'ovo']
